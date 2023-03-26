d0f40dbf08bdab0ca5cd1c0ed433557a6a588011
Rahul Vishwakarma <103241598+rahulvishwakarma0111@users.noreply.github.com>
2023-03-26 23:45:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function Hello(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000);
+	});
+}
+Hello().then(data=>{
+   document.getElementById("output").textContent=data;
+});
